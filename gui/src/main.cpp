e3ac9d8af640196f1015fd52afac41c884ea6597
Vn0m <juanpapolo123@gmail.com>
2026-05-16 21:28:47 -0400

    add qlementine for style

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -1,8 +1,11 @@
 #include <QApplication>
+#include <oclero/qlementine.hpp>
 #include "main_window.hpp"
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
+    auto* style = new oclero::qlementine::QlementineStyle(&app);
+    QApplication::setStyle(style);
     MainWindow window;
     window.show();
     return app.exec();

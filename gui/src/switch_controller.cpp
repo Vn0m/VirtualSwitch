d7190b6408bb59a988bb93070218137d6cc1a37e
Vn0m <juanpapolo123@gmail.com>
2026-05-30 22:52:31 -0400

    disable replay in client

--- gui/src/switch_controller.cpp
+++ gui/src/switch_controller.cpp
@@ -106,6 +106,8 @@ void SwitchController::buildOvpnConfig() {
         "route 10.0.0.0 255.255.255.0\n"
         "cipher AES-256-CBC\n"
         "auth SHA256\n"
+        "replay-window 0\n"
+        "mute-replay-warnings\n"
         "<secret>\n" +
         key +
         "</secret>\n";
